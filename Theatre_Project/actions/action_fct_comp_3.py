4e3ae227f3a15f9ba7f80009df241a7b3d8d168c
Alaa Ben Fatma <benfatma@f108-11.e-im2ag.ujf-grenoble.fr>
2019-11-26 15:47:21 +0100

    1.6 : DONE.

--- Theatre_Project/actions/action_fct_comp_3.py
+++ Theatre_Project/actions/action_fct_comp_3.py
@@ -13,21 +13,25 @@ class AppFctComp3(QDialog):
         super(QDialog, self).__init__()
         self.ui = uic.loadUi("gui/fct_comp_3.ui", self)
         self.data = data
-
+        self.cursor = self.data.cursor()
+        for row in self.cursor.execute('SELECT catZone FROM LesZones'):
+            self.ui.combo_sql_cat.addItem(row[0])
     # Fonction de mise à jour de l'affichage
     @pyqtSlot()
     def refreshResult(self):
         # TODO 1.6 : fonction à modifier pour remplacer la zone de saisie par une liste de valeurs issues de la BD une fois le fichier ui correspondant mis à jour
-        display.refreshLabel(self.ui.label_fct_comp_3, "")
-        if not self.ui.lineEdit_fct_comp_3.text().strip():
+        
+       # display.refreshLabel(self.ui.combo_sql_cat, "")
+        if not self.ui.combo_sql_cat.currentText():
             self.ui.table_fct_comp_3.setRowCount(0)
-            display.refreshLabel(self.ui.label_fct_comp_3, "Veuillez indiquer un nom de catégorie")
+           # display.refreshLabel(self.ui.combo_sql_cat.currentText(), "Veuillez indiquer un nom de catégorie")
         else:
             try:
                 cursor = self.data.cursor()
                 result = cursor.execute(
                     "SELECT noPlace, noRang, noZone FROM LesZones NATURAL JOIN LesPlaces WHERE catZone = ?",
-                    [self.ui.lineEdit_fct_comp_3.text().strip()])
+                    [self.ui.combo_sql_cat.currentText()])
+               
             except Exception as e:
                 self.ui.table_fct_comp_3.setRowCount(0)
                 display.refreshLabel(self.ui.label_fct_comp_3, "Impossible d'afficher les résultats : " + repr(e))
