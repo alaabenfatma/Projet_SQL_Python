36cf62a272994c5e534404cedd4ce04269300498
Alaa Ben Fatma <benfatma@f203-24.e-im2ag.ujf-grenoble.fr>
2019-11-26 18:21:17 +0100

    2.1: DONE.

--- Theatre_Project/main.py
+++ Theatre_Project/main.py
@@ -12,7 +12,7 @@ from actions.action_fct_comp_1 import AppFctComp1
 from actions.action_fct_comp_2 import AppFctComp2
 from actions.action_fct_comp_3 import AppFctComp3
 from actions.action_fct_comp_4 import AppFctComp4
-
+from actions.action_rep_libre_2_1 import AppRepLibre
 # Classe utilisée pour lancer la fenêtre principale de l'application et définir ses actions
 class AppWindow(QMainWindow):
 
@@ -20,6 +20,8 @@ class AppWindow(QMainWindow):
     changedValue = pyqtSignal()
 
     # TODO 2 : ajouter les fenetres (répertoire gui) et les actions (répertoire actions) correspondant aux 2 items de la partie 2.
+    rep_libre_2_1_dialog = None
+    
     # TODO 3 : ajouter les fenetres (rep. gui) et les actions (rep. actions) correspondant aux 2 items de la partie 3.
 
     # On prévoit des variables pour accueillir les fenêtres supplémentaires
@@ -30,7 +32,6 @@ class AppWindow(QMainWindow):
     fct_comp_2_dialog = None
     fct_comp_3_dialog = None
     fct_comp_4_dialog = None
-
     # Constructeur
     def __init__(self):
 
@@ -105,6 +106,12 @@ class AppWindow(QMainWindow):
     ####################################################################################################################
 
     # TODO 2 : ajouter la définition des méthodes déclenchées lors des clicks sur les boutons de la partie 2
+    def open_rep_libre_2_1(self):
+        if self.rep_libre_2_1_dialog is not None:
+            self.rep_libre_2_1_dialog.close()
+        self.rep_libre_2_1_dialog = AppRepLibre(self.data)
+        self.rep_libre_2_1_dialog.show()
+
     # TODO 3 : ajouter la définition des méthodes déclenchées lors des clicks sur les boutons de la partie 3
 
     # En cas de clic sur le bouton de visualisation des données
@@ -185,6 +192,8 @@ class AppWindow(QMainWindow):
             self.fct_comp_3_dialog.close()
         if (self.fct_comp_4_dialog is not None):
             self.fct_comp_4_dialog.close()
+        if (self.rep_libre_2_1_dialog is not None):
+            self.rep_libre_2_1_dialog.close()
 
         # On ferme proprement la base de données
         self.data.close()
