a6c3eca5ce6371510aff893d52ca0285a2edcc10
JohanDa9u <wassimayari10@gmail.com>
2019-11-30 13:19:54 +0100

    ajout window finished, now for the request

--- Theatre_Project/actions/action_res_ajout.py
+++ Theatre_Project/actions/action_res_ajout.py
@@ -28,8 +28,8 @@ class AppResAjout(QDialog):
             self.ui.comboBox_2.addItem(row[0])
         for row in self.cursor.execute('SELECT DISTINCT noPlace FROM LesPlaces'):
             self.ui.comboBox_3.addItem(str(row[0]))
-        index3 = self.ui.combo_sql_rep.currentIndex()
-        for row in self.cursor.execute('SELECT noRang FROM LesPlaces EXCEPT SELECT noRang FROM LesTickets NATURAL JOIN LesSpectacles WHERE nomSpec LIKE ? AND noPlace = ? AND dateRep = ? ', [self.ui.combo_sql_spec.itemText(index), self.ui.comboBox_3.itemText(index3), self.ui.combo_sql_rep.itemText(index)]):
+        index3 = self.ui.comboBox_3.currentIndex()
+        for row in self.cursor.execute('SELECT noRang FROM LesPlaces EXCEPT SELECT noRang FROM LesTickets NATURAL JOIN LesSpectacles WHERE nomSpec LIKE ? AND noPlace = ? AND dateRep LIKE ? ', [self.ui.combo_sql_spec.itemText(index), self.ui.comboBox_3.itemText(index3), self.ui.combo_sql_rep.itemText(index2)]):
             self.ui.comboBox_4.addItem(str(row[0]))
     # Fonction de mise à jour de l'affichage
     @pyqtSlot()
@@ -46,3 +46,12 @@ class AppResAjout(QDialog):
             #self.ui.table.setRowCount(0)
             #display.refreshLabel(self.ui.label_fct_comp_1, "Impossible d'afficher les résultats : " + repr(e))
             print("Impossible d'afficher les résultats : " + repr(e))
+    
+    @pyqtSlot()
+    def refreshResult2(self):
+        self.ui.comboBox_4.clear()
+        index = self.ui.combo_sql_spec.currentIndex()
+        index2 = self.ui.combo_sql_rep.currentIndex()
+        index3 = self.ui.comboBox_3.currentIndex()
+        for row in self.cursor.execute('SELECT noRang FROM LesPlaces EXCEPT SELECT noRang FROM LesTickets NATURAL JOIN LesSpectacles WHERE nomSpec LIKE ? AND noPlace = ? AND dateRep = ? ', [self.ui.combo_sql_spec.itemText(index), self.ui.comboBox_3.itemText(index3), self.ui.combo_sql_rep.itemText(index2)]):
+            self.ui.comboBox_4.addItem(str(row[0]))
